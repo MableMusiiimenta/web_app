2fe310992da88065c85630d7b302438e31c6a5b8
Mable Musiimenta <mmusiimenta@student.refactory.academy>
2024-05-21 09:51:36 +0300

    added a few more vaidations

--- daystarapp/forms.py
+++ daystarapp/forms.py
@@ -38,7 +38,7 @@ class BabyForm(forms.ModelForm):
             "first_name": "First Name",
             "last_name": "Last Name",
             "gender": "Gender",
-            "age": "Age",
+            "age": "Age In Years",
             "location": "Location",
             "parent_name": "Parent's Name",
             "parent_contact": "Parent's Contact",
